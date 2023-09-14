9330b5fe7de2087665b446e7462296540b7f37cf
shunhamm <shun.022885@gmail.com>
2023-09-14 17:53:52 +0900

    make convert_length script and test

--- ruby-book/lib/convert_length.rb
+++ ruby-book/lib/convert_length.rb
@@ -1,3 +1,4 @@
-def convert_length(n, m, i)
-  39.37
+def convert_length(length, unit_from, unit_to)
+  units = { 'm' => 1.0, 'ft' => 3.28, 'in' => 39.37 }
+  (length / units[unit_from]) * units[unit_to]
 end
--- ruby-book/test/convert_length_test.rb
+++ ruby-book/test/convert_length_test.rb
@@ -4,5 +4,6 @@ require_relative '../lib/convert_length'
 class ConvertLengthTest < Minitest::Test
   def test_convert_length
     assert_equal 39.37, convert_length(1, 'm', 'in')
+    assert_equal 3.28, convert_length(39.37, 'in', 'ft')
   end
 end
