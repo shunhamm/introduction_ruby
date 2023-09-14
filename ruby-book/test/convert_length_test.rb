16060209dde848279dba3177f33c2e985fefd66c
shunhamm <shun.022885@gmail.com>
2023-09-14 17:57:39 +0900

    make convert_length script and test

--- ruby-book/test/convert_length_test.rb
+++ ruby-book/test/convert_length_test.rb
@@ -3,7 +3,7 @@ require_relative '../lib/convert_length'
 
 class ConvertLengthTest < Minitest::Test
   def test_convert_length
-    assert_equal 39.37, convert_length(1, :m, :in)
-    assert_equal 3.28, convert_length(39.37, :in, :ft)
+    assert_equal 39.37, convert_length(1, from: :m, to: :in)
+    assert_equal 3.28, convert_length(39.37, from: :in, to: :ft)
   end
 end
