eaa78eee7f140981624d9b9152a018aabd5ca220
shunhamm <shun.022885@gmail.com>
2023-09-14 12:27:32 +0900

    convert_lengthのためのテストコードを作成

---
+++ ruby-book/lib/convert_length.rb
@@ -0,0 +1,3 @@
+def convert_length(n, m, i)
+  39.37
+end
--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -5,3 +5,12 @@ def to_hex(r, g, b)
   end
   hex
 end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  [r, g, b].map do |s|
+    s.hex
+  end
+end
