89a274fe639c23f0a073f3677044d00aea40d975
shunhamm <shun.022885@gmail.com>
2023-09-19 15:30:12 +0900

    テストケースの追加

--- ruby-book/test/gate_test.rb
+++ ruby-book/test/gate_test.rb
@@ -15,9 +15,21 @@ class GateTest < Minitest::Test
     assert @juso.exit(ticket)
   end
 
+  def test_umeda_to_mikuni
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+    assert @mikuni.exit(ticket)
+  end
+
   def test_umeda_to_mikuni_when_fare_is_not_enough
     ticket = Ticket.new(160)
     @umeda.enter(ticket)
     refute @mikuni.exit(ticket)
+  end
+
+  def test_juso_to_mikuni
+    ticket = Ticket.new(160)
+    @juso.enter(ticket)
+    assert @mikuni.exit(ticket)
   end
 end
